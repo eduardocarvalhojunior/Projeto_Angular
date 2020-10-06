44820cea6f4e32e9fb4e19062f658ccfa167b7f2
eduardocarvalhojunior <junior.baller@hotmail.com>
2020-10-05 22:03:00 -0300

    Configurando Botão

--- frontend/src/app/views/product-crud/product-crud.component.ts
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-product-crud',
@@ -9,10 +10,12 @@ export class ProductCrudComponent implements OnInit {
 
   
 
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit(): void {
   }
   
-
+  navigateToProductCreate(): void {
+    this.router.navigate(['/products/create'])
+  }
 }
